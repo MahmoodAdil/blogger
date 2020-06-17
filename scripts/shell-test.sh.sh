3dabf2e46e4aabc4bec0295e28ea7e01f5e8c6a5
Adil Mahmood <adil.mahmood@outlook.ie>
2020-06-17 16:51:36 +0100

    print env var

--- scripts/shell-test.sh.sh
+++ scripts/shell-test.sh.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # This is a shell test file!
 echo "This line printing from scripts/ jenkins-bootstrap-submetadata file"	# This is demo!
+echo "$SL_USERNAME"
+echo "$SL_API_KEY"
+echo "$hostnames"
