1297e468dcbad5bdf303ebf42e6e8b97d07abf10
Adil Mahmood <adil.mahmood@outlook.ie>
2020-06-18 00:00:01 +0100

    change hostfile var name

--- scripts/shell-test.sh.sh
+++ scripts/shell-test.sh.sh
@@ -18,9 +18,9 @@ do
 	printf '%s\n' "$line"
 done <"$file"
 
-file="./$hostnames"
+hostfile="./$hostnames"
 while IFS= read -r line
 do
         # display $line or do somthing with $line
 	printf '%s\n' "$line"
-done <"$file"
+done <"$hostfile"
